024bb0d233fdaf8ecfe6d9edfd9a4173d3a33bf0
Michael Twitt <michael.twitt@hotmail.com>
2020-02-16 18:42:02 +1000

    Added Jumbotron Component

---
+++ src/components/Jumbotron.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Container, Jumbotron as Jumbo } from "react-bootstrap";
+import styled from "styled-components";
+
+export const Jumbotron = () => (
+  <div>
+    <p></p>
+  </div>
+);
